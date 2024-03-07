6ceee2841f2535bacb97d1db22fc4a299caa814a
Felipe Motta <velox4x@gmail.com>
2024-03-07 12:48:05 -0300

    Jogada especial roque pequeno e grande

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -21,6 +21,12 @@ namespace xadrez
             return p == null || p.cor != cor;
         }
 
+        private bool testeTorreParaToque (Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
+        }
+
         public override bool[,] movimentosPossiveis()
         {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
@@ -83,6 +89,34 @@ namespace xadrez
                 mat[pos.linha, pos.coluna] = true;
             }
 
+            // #JogadaEspecial Roque
+            if (qteMovimentos == 0 && !partida.xeque)
+            {
+                //#jogadaespecial roque pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaToque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+                //#jogadaespecial roque grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna + 4);
+                if (testeTorreParaToque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+            }
             return mat;
         } 
     }
